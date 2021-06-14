4262cd16918db026c90edba238558f0d8839d7a7
Jahongir Nagmatov <jahongirnagmatov@gmail.com>
2021-06-14 19:20:10 +0500

    install redux and create product States

---
+++ frontend/src/actions/productActions.js
@@ -0,0 +1,13 @@
+import {
+    PRODUCT_LIST_REQUEST,
+    PRODUCT_LIST_SUCCESS,
+    PRODUCT_LIST_FAIL
+} from '../constants/productConstants'
+
+const listProducts = () => async (dispatch) => {
+    try{
+        dispatch{ type: PRODUCT_LIST_REQUEST}
+    } catch(error){
+
+    }
+}
