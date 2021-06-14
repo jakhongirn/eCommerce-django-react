d69bf9b650f5a944a217bfc0429e626acf8fd083
Jahongir Nagmatov <jahongirnagmatov@gmail.com>
2021-06-14 16:14:54 +0500

    finish django part

---
+++ backend/base/admin.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+from .models import OrderItem, Product, ShippingAddress, Review, Order
+# Register your models here.
+
+admin.site.register(Product)
+admin.site.register(Review)
+admin.site.register(Order)
+admin.site.register(OrderItem)
+admin.site.register(ShippingAddress )
