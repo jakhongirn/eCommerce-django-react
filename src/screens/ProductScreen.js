1362ffe1e17770baec68635784073c1257e905ab
Jahongir Nagmatov <jahongirnagmatov@gmail.com>
2021-06-14 12:02:21 +0500

    connects frontend to backend through API

--- src/screens/ProductScreen.js
+++ src/screens/ProductScreen.js
@@ -1,11 +1,25 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 import { Link } from "react-router-dom";
 import { Row, Col, Image, ListGroup, Button, Card } from "react-bootstrap";
 import Review from "../components/Rating";
 import products from "../products";
+import axios from 'axios'
 
 function ProductScreen({ match }) {
-  const product = products.find((p) => p._id === match.params.id);
+
+  const [product, setProduct] = useState([])
+
+  useEffect(()=> {
+
+      async function fetchProduct(){
+
+          const {data} = await axios.get(`/api/products/${match.params.id}`)  /* Go to package.json */
+          setProduct(data)
+      }
+      fetchProduct()
+  }, [])
+
+ /*  const product = products.find((p) => p._id === match.params.id); */ /* static file  */
   return (
     <div>
       <Link to="/" className="btn btn-light my-3">
