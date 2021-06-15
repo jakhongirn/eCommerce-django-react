f79e2851d282485281e7d4ce776edd33ce01423a
Jahongir Nagmatov <jahongirnagmatov@gmail.com>
2021-06-15 10:25:25 +0500

    firing off response message (loading, error, etc)

--- frontend/src/actions/productActions.js
+++ frontend/src/actions/productActions.js
@@ -5,12 +5,12 @@ import {
     PRODUCT_LIST_FAIL
 } from '../constants/productConstants'
 
-const listProducts = () => async (dispatch) => {
+export const listProducts = () => async (dispatch) => {
     try{
         dispatch({ type: PRODUCT_LIST_REQUEST})
 
         const {data} = await axios.get('/api/products/')  /* Go to package.json */
-            setProducts(data)
+            
 
         dispatch({
             type: PRODUCT_LIST_SUCCESS,
--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,27 +1,38 @@
 import React, {useState, useEffect} from 'react'
+import { useDispatch, useSelector,  } from 'react-redux'
 import {Row, Col} from 'react-bootstrap'
 import products from '../products'
 import Product from '../components/Product'
+import { listProducts } from '../actions/productActions'
 
 
 function HomeScreen() {
     
+    const dispatch = useDispatch()
+    const productList = useSelector(state => state.productList)
+    const { error, loading, products } = productList
 
     useEffect(()=> {
-
+        dispatch(listProducts())
         
     }, [])
+    
     return (
         <div>
 
             <h1>Latest products</h1>
-            <Row>
-                {products.map(product => (
-                    <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
-                        <Product product={product} />
-                    </Col>
-                ))}
-            </Row>
+            {loading ? <h2>Loading ...</h2>
+                : error ? <h3>{error}</h3>
+                    : 
+                    <Row>
+                    {products.map(product => (
+                        <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
+                            <Product product={product} />
+                        </Col>
+                    ))}
+            </Row>}
+
+            
             
         </div>
     )
