2ef158887876f037c152388c2864e85296cb090e
Jahongir Nagmatov <jahongirnagmatov@gmail.com>
2021-06-15 15:24:16 +0500

    add quantity select function

--- frontend/src/screens/ProductScreen.js
+++ frontend/src/screens/ProductScreen.js
@@ -1,37 +1,45 @@
-import React, {useState, useEffect} from "react";
-import {useDispatch, useSelector} from 'react-redux'
+import React, { useState, useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
 import { Link } from "react-router-dom";
-import { Row, Col, Image, ListGroup, Button, Card } from "react-bootstrap";
+import {
+  Row,
+  Col,
+  Image,
+  ListGroup,
+  Button,
+  Card,
+  Form,
+} from "react-bootstrap";
 import Review from "../components/Rating";
 import Loader from "../components/Loader";
 import Message from "../components/Message";
 import products from "../products";
- 
-import { listProductDetails } from '../actions/productActions'
 
-
+import { listProductDetails } from "../actions/productActions";
 
 function ProductScreen({ match }) {
-  const dispatch = useDispatch()
-  const productDetails = useSelector(state => state.productDetails)
-  const { loading, error, product} = productDetails
+  const [qty, setQty] = useState(1);
+  const dispatch = useDispatch();
+  const productDetails = useSelector((state) => state.productDetails);
+  const { loading, error, product } = productDetails;
 
-  useEffect(()=> {
-    dispatch(listProductDetails(match.params.id))
+  useEffect(() => {
+    dispatch(listProductDetails(match.params.id));
+  }, [dispatch, match]);
 
-  }, [dispatch, match])
-
- /*  const product = products.find((p) => p._id === match.params.id); */ /* static file  */
+  /*  const product = products.find((p) => p._id === match.params.id); */ /* static file  */
   return (
     <div>
-      <Link to="/" className="btn btn-light my-3">Go back</Link>
+      <Link to="/" className="btn btn-light my-3">
+        Go back
+      </Link>
 
-      {loading ?
+      {loading ? (
         <Loader />
-        : error
-          ? <Message variant='danger'>{error}</Message>
-        :(
-          <Row>
+      ) : error ? (
+        <Message variant="danger">{error}</Message>
+      ) : (
+        <Row>
           <Col md={6}>
             <Image src={product.image} alt={product.name} fluid />
           </Col>
@@ -47,9 +55,11 @@ function ProductScreen({ match }) {
                   color={"#f8e825"}
                 ></Review>
               </ListGroup.Item>
-  
+
               <ListGroup.Item>Price: {product.price}</ListGroup.Item>
-              <ListGroup.Item>Description: {product.description}</ListGroup.Item>
+              <ListGroup.Item>
+                Description: {product.description}
+              </ListGroup.Item>
             </ListGroup>
           </Col>
           <Col md={3}>
@@ -64,7 +74,7 @@ function ProductScreen({ match }) {
                   </Row>
                 </ListGroup.Item>
               </ListGroup>
-  
+
               <ListGroup variant="flush">
                 <ListGroup.Item>
                   <Row>
@@ -72,7 +82,28 @@ function ProductScreen({ match }) {
                     <Col>{product.countInStock}</Col>
                   </Row>
                 </ListGroup.Item>
-  
+
+                {product.countInStock > 0 && (
+                  <ListGroup.Item>
+                    <Row>
+                      <Col>Qty</Col>
+                      <Col xs="auto" className="my-1">
+                        <Form.Control
+                          as="select"
+                          value={qty}
+                          onChange={(e) => setQty(e.target.value)}
+                        >
+                          {[...Array(product.countInStock).keys()].map((x) => (
+                            <option key={x + 1} value={x + 1}>
+                              {x + 1}
+                            </option>
+                          ))}
+                        </Form.Control>
+                      </Col>
+                    </Row>
+                  </ListGroup.Item>
+                )}
+
                 <ListGroup.Item>
                   <Row>
                     <Button
@@ -87,10 +118,8 @@ function ProductScreen({ match }) {
               </ListGroup>
             </Card>
           </Col>
-        </Row> 
-        )
-        }
-     
+        </Row>
+      )}
     </div>
   );
 }
