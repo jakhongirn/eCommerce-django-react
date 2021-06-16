fb8643596d06d56f3c25673b4a91df839ef5acd9
Jahongir Nagmatov <jahongirnagmatov@gmail.com>
2021-06-16 10:01:24 +0500

    remove all cart files to debug

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,7 +4,7 @@ import Header from "./components/Header";
 import Footer from "./components/Footer";
 import HomeScreen from "./screens/HomeScreen";
 import ProductScreen from "./screens/ProductScreen";
-import CartScreen from './screens/CartScreen'
+
 
 function App() {
   return (
@@ -14,7 +14,6 @@ function App() {
         <Container>
           <Route path="/" component={HomeScreen} exact />
           <Route path="/product/:id" component={ProductScreen} />
-          <Route path="/cart/:id?" component={CartScreen} />
         </Container>
       </main>
       <Footer />
