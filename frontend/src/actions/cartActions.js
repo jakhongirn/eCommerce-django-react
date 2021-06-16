f750043d545f3fc95e44fc1a4a5c738ed29ffe5b
Jahongir Nagmatov <jahongirnagmatov@gmail.com>
2021-06-16 11:27:34 +0500

    add cartlist to cartScreen

--- frontend/src/actions/cartActions.js
+++ frontend/src/actions/cartActions.js
@@ -8,6 +8,7 @@ export const addToCart = (id, qty) => async (dispatch, getState) => {
         type: CART_ADD_ITEM,
         payload: {
             product: data._id,
+            name: data.name,
             image: data.image,
             price: data.price,
             countInStock: data.countInStock,
--- frontend/src/screens/CartScreen.js
+++ frontend/src/screens/CartScreen.js
@@ -2,7 +2,7 @@ import React, {useEffect} from 'react'
 import { Link } from 'react-router-dom'
 import { useDispatch, useSelector } from 'react-redux'
 import { Row, Col, ListGroup, Image, Form, Button, Card } from 'react-bootstrap'
-import { Message } from '../components/Message'
+import  Message  from '../components/Message'
 import {addToCart} from '../actions/cartActions'
 
 
@@ -16,6 +16,7 @@ function CartScreen({match, location, history}) {
     const {cartItems} = cart 
     console.log('cartItems', cartItems)
     
+    
 
     useEffect(() => {
         if(productId) {
@@ -25,7 +26,36 @@ function CartScreen({match, location, history}) {
 
     return (
         <div>
-            Cart
+           <Col md={8}>
+               <h1>Shopping Cart</h1>
+               {cartItems.length === 0 ? (
+                   <Message variant='info'>
+                       Your cart is empty <Link to='/'>Go back</Link>
+                   </Message>
+               ): (
+                   <ListGroup variant='flush'>
+                        {cartItems.map(item => (
+                            <ListGroup.Item key={item.product}>
+                                <Row>
+                                    <Col md={2}>
+                                        <Image src={item.image} alt={item.name} fluid rounded/>
+                                    </Col>
+                                    <Col md={3}>
+                                        <Link to={`/product/${item.product}`}>{item.name}</Link>
+                                    </Col>
+
+                                    <Col md={2}>
+                                        ${item.price}
+                                    </Col>
+                                </Row>
+                            </ListGroup.Item>
+                        ))}
+                   </ListGroup>
+               )}
+           </Col>
+           <Col md={4}>
+           
+           </Col>
         </div>
     )
 }
