28230d52efb67833e2320599f5a307024769e3a5
Jahongir Nagmatov <jahongirnagmatov@gmail.com>
2021-06-16 11:08:26 +0500

    complete addToCart function (3.)

--- frontend/src/screens/CartScreen.js
+++ frontend/src/screens/CartScreen.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, {useEffect} from 'react'
 import { Link } from 'react-router-dom'
 import { useDispatch, useSelector } from 'react-redux'
 import { Row, Col, ListGroup, Image, Form, Button, Card } from 'react-bootstrap'
@@ -11,6 +11,11 @@ function CartScreen({match, location, history}) {
     const qty = location.search ? Number(location.search.split('=')[1]): 1
     
     const dispatch = useDispatch()
+
+    const cart = useSelector(state => state.cart)
+    const {cartItems} = cart 
+    console.log('cartItems', cartItems)
+    
 
     useEffect(() => {
         if(productId) {
