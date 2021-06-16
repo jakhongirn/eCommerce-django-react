245f37ecab4fde16896c6bbe8f0e045f322d97b9
Jahongir Nagmatov <jahongirnagmatov@gmail.com>
2021-06-16 10:49:24 +0500

    add qty, productID to useEffect to state

--- frontend/src/screens/CartScreen.js
+++ frontend/src/screens/CartScreen.js
@@ -10,6 +10,13 @@ function CartScreen({match, location, history}) {
     const productId = match.params.id
     const qty = location.search ? Number(location.search.split('=')[1]): 1
     
+    const dispatch = useDispatch()
+
+    useEffect(() => {
+        if(productId) {
+            dispatch(addToCart(productId, qty))
+        }
+    }, [dispatch, productId, qty])
 
     return (
         <div>
