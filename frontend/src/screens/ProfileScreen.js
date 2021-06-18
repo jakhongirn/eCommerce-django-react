62efb9e5abd1627ff0db20c4a033757b3db54f1e
Jahongir Nagmatov <jahongirnagmatov@gmail.com>
2021-06-18 18:00:51 +0500

    update userDetails func

--- frontend/src/screens/ProfileScreen.js
+++ frontend/src/screens/ProfileScreen.js
@@ -8,10 +8,52 @@ import { getUserDetails } from "../actions/userActions";
 import { register } from "../actions/userActions";
 
 export default function ProfileScreen({ history }) {
+
+    const [name, setName] = useState("");
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const [confirmPassword, setConfirmPassword] = useState("");
+    const [message, setMessage] = useState("");
+
+    const dispatch = useDispatch();
+
+
+    const userDetails = useSelector((state) => state.userDetails);
+    const { error, loading, user } = userDetails;
+    const userLogin = useSelector((state) => state.userLogin);
+    const { userInfo } = userLogin;
+
+    useEffect(() => {
+        if (!userInfo) {
+        history.push('/login');
+        }else {
+            if(!user || !user.name){
+                dispatch(getUserDetails('profile'))
+
+            }else{
+                setName(user.name)
+                setEmail(user.email)
+            }
+        }
+    }, [dispatch, history, userInfo, user]);
+
+    const submitHandler = (e) => {
+        e.preventDefault();
+
+        if(password != confirmPassword) {
+            setMessage('Passwords do not match')
+        } else {
+            console.log('Updating..')
+        }
+        
+    };
+
   return (
     <Row>
       <Col md={3}>
         <h2>User Profile</h2>
+
+        
       </Col>
       <Col md={9}>
         <h2>My orders</h2>
