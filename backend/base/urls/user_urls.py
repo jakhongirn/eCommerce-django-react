4b9d26ad71c854f3abd14ab22af320a41ae6658d
Jahongir Nagmatov <jahongirnagmatov@gmail.com>
2021-06-18 16:12:38 +0500

    update user_views and user_urls

--- backend/base/urls/user_urls.py
+++ backend/base/urls/user_urls.py
@@ -8,6 +8,8 @@ urlpatterns = [
     path('register/', views.registerUser, name="register"),
     path('profile/', views.getUserProfile, name="user-profile"),
 
+    path('profile/update', views.updateUserProfile, name="user-profile-update"),
+
     path('users/', views.getUsers, name="users"),
     path('login/', views.MyTokenObtainPairView.as_view(), name='token_obtain_pair'),
 ]
