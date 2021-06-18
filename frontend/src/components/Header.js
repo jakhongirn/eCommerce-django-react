27b015938babf53e08a2824654185b17ad06ee19
Jahongir Nagmatov <jahongirnagmatov@gmail.com>
2021-06-18 13:05:05 +0500

    complete logout action functionality

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -3,14 +3,17 @@ import {useDispatch, useSelector} from 'react-redux'
 
 import { Navbar, Nav, Container, Row, NavDropdown } from "react-bootstrap";
 import { LinkContainer } from "react-router-bootstrap";
+import {logout} from '../actions/userActions'
 
 function Header() {
 
   const userLogin = useSelector(state => state.userLogin)
   const { userInfo } = userLogin
 
+  const dispatch = useDispatch();
+
   const logoutHandler = () => {
-    console.log('Logout')
+    dispatch(logout())
   }
 
   return (
