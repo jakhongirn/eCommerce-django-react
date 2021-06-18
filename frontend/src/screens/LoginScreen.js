558baabea3cde076ede201a4a4490f74253eaa37
Jahongir Nagmatov <jahongirnagmatov@gmail.com>
2021-06-18 12:00:08 +0500

    Create form for signing in with bootstrap

--- frontend/src/screens/LoginScreen.js
+++ frontend/src/screens/LoginScreen.js
@@ -1,21 +1,55 @@
-import React, {useState, useEffect} from 'react'
-import {Link} from 'react-router-dom'
-import { Form, Button, Row, Col } from 'react-bootstrap'
-import { useDispatch, useSelector } from 'react-redux'
-import Loader from "../components/Loader"
-import Message from '../components/Message'
-import {login} from '../actions/userActions'
+import React, { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
+import { Form, Button, Row, Col } from "react-bootstrap";
+import { useDispatch, useSelector } from "react-redux";
+import Loader from "../components/Loader";
+import Message from "../components/Message";
+import FormContainer from "../components/FormContainer";
+import { login } from "../actions/userActions";
 
 function LoginScreen() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
 
-    const [email, setEmail] = useState('')
-    const [password, setPassword] = useState('')
+  const submitHandler = (e) => {
+    e.preventDefault();
+    console.log("Submitted");
+  };
 
-    return (
-        <div>
-            Login Screen
-        </div>
-    )
+  return (
+    <FormContainer>
+      <h1>Sign in</h1>
+
+      <Form onSubmit={submitHandler}>
+        <Form.Group controlId="email">
+          <Form.Label>Email Address</Form.Label>
+          <Form.Control
+            type="email"
+            placeHolder="Enter Email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          ></Form.Control>
+        </Form.Group>
+
+        <Form.Group controlId="password">
+          <Form.Label>Password here</Form.Label>
+          <Form.Control
+            type="password"
+            placeHolder="Enter Password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          ></Form.Control>
+        </Form.Group>
+
+        <Button type="submit" variant="primary">
+          Sign in
+        </Button>
+      </Form>
+
+        
+
+    </FormContainer>
+  );
 }
 
-export default LoginScreen
+export default LoginScreen;
